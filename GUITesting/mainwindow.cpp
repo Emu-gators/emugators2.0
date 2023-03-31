c1d2fa73823c897986893aac9c6f4d9e74138684
chris3764 <69045948+chris3764@users.noreply.github.com>
2023-03-31 12:29:25 -0400

    updated file paths of ROMS and images

--- GUITesting/mainwindow.cpp
+++ GUITesting/mainwindow.cpp
@@ -25,71 +25,71 @@ MainWindow::~MainWindow()
 
 void MainWindow::loadROMImages()
 {
-    QImage img0("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/BalloonFight(Japan).jpg");
+    QImage img0("../../../ROMS/emugator/ROM_Carts/BalloonFight(Japan).jpg");
     QImage scale0 = img0.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale0);
-    QImage img1("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/B-Wings(Japan).jpg");
+    QImage img1("../../../ROMS/emugator/ROM_Carts/B-Wings(Japan).jpg");
     QImage scale1 = img1.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale1);
-    QImage img2("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/CrisisForce(Japan).jpg");
+    QImage img2("../../../ROMS/emugator/ROM_Carts/CrisisForce(Japan).jpg");
     QImage scale2 = img2.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale2);
-    QImage img3("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/DigDug(Japan).jpg");
+    QImage img3("../../../ROMS/emugator/ROM_Carts/DigDug(Japan).jpg");
     QImage scale3 = img3.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale3);
-    QImage img4("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/DrMario(Japan,USA).jpg");
+    QImage img4("../../../ROMS/emugator/ROM_Carts/DrMario(Japan,USA).jpg");
     QImage scale4 = img4.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale4);
-    QImage img5("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/Final Fantasy III (Japan).jpg");
+    QImage img5("../../../ROMS/emugator/ROM_Carts/Final Fantasy III (Japan).jpg");
     QImage scale5 = img5.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale5);
-    QImage img6("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/Gradius (Japan).jpg");
+    QImage img6("../../../ROMS/emugator/ROM_Carts/Gradius (Japan).jpg");
     QImage scale6 = img6.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale6);
-    QImage img7("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/Gradius II (Japan).jpg");
+    QImage img7("../../../ROMS/emugator/ROM_Carts/Gradius II (Japan).jpg");
     QImage scale7 = img7.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale7);
-    QImage img8("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/Ice Climber (Japan).jpg");
+    QImage img8("../../../ROMS/emugator/ROM_Carts/Ice Climber (Japan).jpg");
     QImage scale8 = img8.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale8);
-    QImage img9("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/LegendOfZelda2_JP.jpg");
+    QImage img9("../../../ROMS/emugator/ROM_Carts/LegendOfZelda2_JP.jpg");
     QImage scale9 = img9.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale9);
-    QImage img10("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/Pac-Man (Japan).jpg");
+    QImage img10("../../../ROMS/emugator/ROM_Carts/Pac-Man (Japan).jpg");
     QImage scale10 = img10.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale10);
-    QImage img11("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/Super Mario Bros. 3 (Japan).jpg");
+    QImage img11("../../../ROMS/emugator/ROM_Carts/Super Mario Bros. 3 (Japan).jpg");
     QImage scale11 = img11.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale11);
-    QImage img12("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/emugator/ROM_Carts/superMarioBros.jpg");
+    QImage img12("../../../ROMS/emugator/ROM_Carts/superMarioBros.jpg");
     QImage scale12 = img12.scaled(100,100,Qt::KeepAspectRatio);
     roms.push_back(scale12);
 
-    QImage background("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/background.jpg");
+    QImage background("../../../ROMS/background.jpg");
     QImage scaleBack = background.scaled(500,750,Qt::KeepAspectRatio);
     ui->background->setPixmap(QPixmap::fromImage(scaleBack));
 
-    QImage games("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/lowresbookshelfedit.png");
+    QImage games("../../../ROMS/lowresbookshelfedit.png");
     QImage scaleGame = games.scaled(400,125,Qt::KeepAspectRatio);
     ui->games->setPixmap(QPixmap::fromImage(scaleGame));
 
-    QImage famicom("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/Nintendo-Famicom-Disk-System.png");
+    QImage famicom("../../../ROMS/Nintendo-Famicom-Disk-System.png");
     QImage scaleFam = famicom.scaled(360,300,Qt::KeepAspectRatio);
     ui->famicom->setPixmap(QPixmap::fromImage(scaleFam));
 
-    QImage shelf("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/shelfedit.jpg");
+    QImage shelf("../../../ROMS/shelfedit.jpg");
     QImage scaleShelf = shelf.scaled(450,65,Qt::KeepAspectRatio);
     ui->shelf->setPixmap(QPixmap::fromImage(scaleShelf));
     ui->shelf->lower();
     ui->background->lower();
 
-    QImage left("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/leftArrow.png");
+    QImage left("../../../ROMS/leftArrow.png");
     QIcon leftButton(QPixmap::fromImage(left));
     ui->previousButton->setIcon(leftButton);
-    ui->previousButton->setIconSize(QPixmap::fromImage(left).rect().size());
+    //ui->previousButton->setIconSize(QPixmap::fromImage(left).rect().size());
 
 
-    QImage right("C:/Users/cjwal/Documents/Spring-23/CEN3907/ROMS/rightArrow.png");
+    QImage right("../../../ROMS/rightArrow.png");
     QIcon rightButton(QPixmap::fromImage(right));
     ui->nextButton->setIcon(rightButton);
 }
@@ -142,7 +142,7 @@ void MainWindow::on_previousButton_clicked()
 
 void MainWindow::mousePressEvent(QMouseEvent *event)
 {
-    if (ui->label->geometry().contains(event->globalPosition().x(),event->globalPosition().y())) {
+    if (ui->label->geometry().contains(event->globalPos().x(),event->globalPos().y())) {
 
         QDrag *drag = new QDrag(this);
 
