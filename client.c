3ca7fd9dc1c2251a82c66e55165199d22c451e70
chris3764 <69045948+chris3764@users.noreply.github.com>
2023-09-07 11:30:50 -0400

    Attempt to implement socket code in GUI and FCEUX

--- client.c
+++ client.c
@@ -15,22 +15,22 @@ int main(int argc, char const* argv[])
 	struct sockaddr_in serv_addr;
 	char* hello = "Hello from client";
 	char buffer[1024] = { 0 };
-	if ((client_fd = socket(AF_LOCAL, SOCK_STREAM, 0)) < 0) {
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Socket creation error \n");
 		return -1;
 	}
 
-	serv_addr.sin_family = AF_LOCAL;
+	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
 
 	// Convert IPv4 and IPv6 addresses from text to binary
 	// form
-	/*if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
 		<= 0) {
 		printf(
 			"\nInvalid address/ Address not supported \n");
 		return -1;
-	}*/
+	}
 
 	if ((status
 		= connect(client_fd, (struct sockaddr*)&serv_addr,
--- src/drivers/Qt/main.cpp
+++ src/drivers/Qt/main.cpp
@@ -17,6 +17,18 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+//Socket code includes
+//This client file utilizes code from https://www.geeksforgeeks.org/socket-programming-cc/
+
+// Client side C/C++ program to demonstrate Socket
+// programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <QApplication>
@@ -95,6 +107,45 @@ static bool showSplashScreen(void)
 
 int main( int argc, char *argv[] )
 {
+	//Socket Client Code
+	int status, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	char* hello = "Hello from client";
+	char buffer[1024] = { 0 };
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if ((status
+		= connect(client_fd, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(client_fd, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+	valread = read(client_fd, buffer, 1024);
+	printf("%s\n", buffer);
+
+	// closing the connected socket
+	close(client_fd);
+    //End of client code
+
+
 	int retval = 0;
 
 	fceuWrapperPreInit(argc, argv);
