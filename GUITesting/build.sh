7f3aca387e9d12f9b8263add13761aed5229f5c5
Jeya <jeyaiyadurai@ufl.edu>
2023-10-06 18:23:16 -0400

    .

--- GUITesting/build.sh
+++ GUITesting/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 echo "Build"
+echo $QMAKE_PATH
 echo $QMAKE_PATH -project "QT += gui widgets multimedia" "LIBS += /usr/local/lib/liblgpio.so"
 $QMAKE_PATH -project "QT += gui widgets multimedia" "LIBS += /usr/local/lib/liblgpio.so"
 echo $QMAKE_PATH GUITesting.pro
