862aff97626213b019a12106251081cd16233a9c
Jeya <jeyaiyadurai@ufl.edu>
2023-10-06 18:32:25 -0400

    .

--- GUITesting/build.sh
+++ GUITesting/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-echo "Build"
+if[$#==1]; then
+    QMAKE_PATH = $1
+else
+    QMAKE_PATH = $QMAKE_PATH
+fi 
 echo $QMAKE_PATH
+echo "Build"
 echo $QMAKE_PATH -project "QT += gui widgets multimedia" "LIBS += /usr/local/lib/liblgpio.so"
 $QMAKE_PATH -project "QT += gui widgets multimedia" "LIBS += /usr/local/lib/liblgpio.so"
 echo $QMAKE_PATH GUITesting.pro
