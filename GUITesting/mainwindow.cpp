21d5feba4572bd27ffb7ace08cd5f4993589e203
Jeya Iyadurai <jay.iyadurai@gmail.com>
2023-10-28 17:36:16 -0400

    .

--- GUITesting/mainwindow.cpp
+++ GUITesting/mainwindow.cpp
@@ -308,7 +308,7 @@ void MainWindow::loadGUIImages()
     QImage scaleHelp = help.scaled(64,64,Qt::KeepAspectRatio);
     QIcon ButtonIcon(QPixmap::fromImage(scaleHelp));
     ui->helpButton->setIcon(ButtonIcon);
-    button->setIconSize(pixmap.rect().size());
+    button->setIconSize(scaleHelp.rect().size());
 
     /* Setting tooltips */
     ui->famicom->setToolTip("Drag and drop one of the cartridges above to start playing!");
